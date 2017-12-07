48ae11624aa578b1f7a4779cb123ba92c0aef1c8
Mateusz Mikolajski <mateusz.mikolajski@gmail.com>
2017-12-07 17:24:23 +0100

    initial put web

---
+++ notesweb/tsScripts/app.ts
@@ -0,0 +1,19 @@
+﻿import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+@Component({
+    selector: 'my-app',
+    template: `    
+    <h1>sample app to display notifications</h1>
+    <h2>Notifications:</h2>
+    <ul>
+      <li *ngFor="let notification of notifications">
+        {{ notification }}
+      </li>
+    </ul>
+  `
+})
+export class AppComponent {
+    title = 'ASP.NET MVC 5 with Angular 2234';
+    notifications = ['Notification 1', 'Notification 2', 'Notification 3', 'Notification 4'];
+
+}
